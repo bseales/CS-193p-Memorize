dcfc0dc0050f313c6f648e52151f8b21295d76d2
Batson Seales <batson.seales@gmail.com>
2022-10-17 20:28:03 -0500

    Initial Commit

---
+++ CS-193p-Memorize/CS_193p_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS_193p_MemorizeApp.swift
+//  CS-193p-Memorize
+//
+//  Created by Batson Seales on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct CS_193p_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
